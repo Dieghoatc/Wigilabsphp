b33a0303a8d04b4e54445f26b3a2017f44b89bfd
Dieghoatc <dieghoatc@gmail.com>
2019-11-28 16:12:10 -0500

    Probando react

---
+++ web/assets/components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+    return(
+        <div classname = "App">
+            <p>Esta corriendo react hpta</p>
+        </div>
+    )
+}
+export default App;
